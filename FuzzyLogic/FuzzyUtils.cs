836845ec6a8ccadda99366034bd804efea2a4164
Alejandro <tochas2000@yahoo.com.mx>
2017-08-21 12:27:57 -0500

    Fixed: Compatible with Unity3D engine

--- FuzzyLogic/FuzzyUtils.cs
+++ FuzzyLogic/FuzzyUtils.cs
@@ -1,23 +1,41 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace Tochas.FuzzyLogic
 {
     public static class FuzzyUtils
     {
-        public static bool IsGenericParameterValid<T>() where T : struct, IConvertible
+        private static Dictionary<System.Type, bool> _cachedParameters;
+
+        static FuzzyUtils()
         {
-            Type typeT = typeof(T);
-            if (!typeT.IsEnum && Type.GetTypeCode(typeT) != TypeCode.Int32)
+            _cachedParameters = new Dictionary<System.Type, bool>(100);
+        }
+
+        public static bool IsGenericParameterValid<T>() where T : struct, System.IConvertible
+        {
+            /*System.Type typeT = typeof(T);
+            bool result = false;
+            if (_cachedParameters.TryGetValue(typeT, out result))
             {
-                throw new ArgumentException("T must be an int-enumerated type");
+                return result;
             }
+            else
+            {
+                result = typeT.IsEnum && System.Type.GetTypeCode(typeT) == System.TypeCode.Int32;
+                _cachedParameters[typeT] = result;
+                if (!result)
+                {
+                    throw new System.ArgumentException("T must be an int-enumerated type");
+                }
+                return result;
+            }*/
             return true;
         }
 
-        public static T[] GetEnumValues<T>() where T : struct, IConvertible
+        public static T[] GetEnumValues<T>() where T : struct, System.IConvertible
         {
             IsGenericParameterValid<T>();
-            var enumValuesArr = Enum.GetValues(typeof(T));
+            var enumValuesArr = System.Enum.GetValues(typeof(T));
             var enumValues = new T[enumValuesArr.Length];
             for (int i = 0; i < enumValuesArr.Length; i++)
             {
